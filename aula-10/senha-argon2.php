269fce46c9df094e14be375051c5916c94459176
Alexandre Almeida <alee.wxa@gmail.com>
2022-06-11 18:16:47 -0300

    adiciona exemplo de password_verify

--- aula-10/senha-argon2.php
+++ aula-10/senha-argon2.php
@@ -1,7 +1,14 @@
 <?php
 $senha = "123456";
 
-echo password_hash($senha, PASSWORD_DEFAULT) . "<br>";
+password_hash($senha, PASSWORD_DEFAULT) . "<br>";
 
 echo password_hash($senha, PASSWORD_ARGON2I) . "<br>"; // Recomendado!
 echo password_hash($senha, PASSWORD_ARGON2I) . "<br>";
+
+$hash = '$argon2i$v=19$m=65536,t=4,p=1$M2p1YlZWMElBU0VGRXhzeQ$5p55YmK/3VjyB7MWS4EWeJ1cpl1DNUPqFEYQmCVvAcg';
+if (password_verify($senha, $hash)) {
+    echo 'Password is valid!';
+} else {
+    echo 'Invalid password.';
+}
